2fef54d18046aea28c848ac9fe3055fdd0df38c6
Richard Willis <willis.rh@gmail.com>
2016-01-02 18:09:10 +0000

    Add image alias methods and fix tests

--- test/lxd-test.js
+++ test/lxd-test.js
@@ -8,6 +8,7 @@ var lxd = require('../');
 
 var container = 'lxd-test-' + new Date().getTime();
 var image = 'lxd-image-' + new Date().getTime();
+var createdImageFingerprint = null;
 
 describe('Main LXD Api endpoints', function() {
 
@@ -28,14 +29,14 @@ describe('Main LXD Api endpoints', function() {
 
     it('Should return available APIs', function() {
       return client.getApis().then(function(res) {
-        expect(res.status).to.be.equal('Success');
+        expect(res.status_code).to.be.equal(200);
         expect(res.metadata).to.contain('/1.0');
       });
     });
 
     it('Should return server info', function() {
       return client.getServerInfo().then(function(res) {
-        expect(res.status).to.be.equal('Success');
+        expect(res.status_code).to.be.equal(200);
         expect(res.metadata.environment.driver).to.be.equal('lxc');
       });
     });
@@ -46,7 +47,7 @@ describe('Main LXD Api endpoints', function() {
           'core.trust_password': ''
         }
       }).then(function(res) {
-        expect(res.status).to.be.equal('Success');
+        expect(res.status_code).to.be.equal(200);
       });
     });
 
@@ -54,15 +55,15 @@ describe('Main LXD Api endpoints', function() {
 
   describe('Images', function() {
 
-    it('Should create an image', function() {
+
+    it('Should create a local image from a remote image', function() {
       return client.createImage({
-        "public": false,
-        "alias": "foo",
-        "source": {
-          "type": "image",
-          "mode": "pull",
-          "server": "https://images.linuxcontainers.org",
-          "alias": "ubuntu/trusty/amd64"
+        'public': false,
+        source: {
+          type: 'image',
+          mode: 'pull',
+          server: 'https://images.linuxcontainers.org',
+          alias: 'ubuntu/trusty/amd64'
         }
       }).then(function(res) {
         expect(res.status).to.be.equal('OK');
@@ -70,24 +71,62 @@ describe('Main LXD Api endpoints', function() {
       }).then(function(operation) {
         var metadata = JSON.parse(operation).metadata;
         expect(metadata.status_code).to.be.equal(200);
+        createdImageFingerprint = metadata.metadata.fingerprint;
       });
     });
 
+    // /* Not implemented in LXD REST API */
+    // it('Should rename an image', function() {
+    //   return client.renameImage(createdImageFingerprint, {
+    //     'name': 'foo',
+    //   }).then(function(res) {
+    //     console.log(res);
+    //     expect(res.status).to.be.equal('OK');
+    //     return client.waitOperation(res);
+    //   }).then(function(operation) {
+    //     var metadata = JSON.parse(operation).metadata;
+    //     expect(metadata.status_code).to.be.equal(200);
+    //     console.log(metadata);
+    //   });
+    // });
+    //
+
     it('Should list images', function() {
       return client.getImages().then(function(res) {
-        expect(res.status).to.be.equal('Success');
+        expect(res.status_code).to.be.equal(200);
         expect(Boolean(res.metadata.length)).to.be.equal(true);
       });
     });
 
     it('Should get an image\'s info', function() {
     });
+
+    describe('Aliases', function() {
+
+      it('Should create an image alias', function() {
+        var json = {
+          description: 'The alias description',
+          target: createdImageFingerprint,
+          name: image
+        }
+        return client.createImageAlias(json).then(function(res) {
+          expect(res.status_code).to.be.equal(200, res);
+        });
+      });
+
+      it('Should list image aliases', function() {
+        return client.getImageAliases().then(function(res) {
+          expect(res.status_code).to.be.equal(200);
+          expect(res.metadata).to.contain('/1.0/images/aliases/' + image);
+        });
+      });
+    });
   });
 
   describe('Profiles', function() {
     it('Should list profiles', function() {
       return client.getProfiles().then(function(res) {
-        expect(res.status).to.be.equal('Success');
+        expect(res.status_code).to.be.equal(200);
         expect(Boolean(res.metadata.length)).to.be.equal(true);
       });
     })
@@ -101,7 +140,7 @@ describe('Main LXD Api endpoints', function() {
         profiles: ['default'],
         source: {
           type: 'image',
-          alias: 'ubuntu/trusty/amd64'
+          alias: image
         }
       }).then(function(res) {
         expect(res.status).to.be.equal('OK');
@@ -112,14 +151,14 @@ describe('Main LXD Api endpoints', function() {
 
     it('Should list containers', function() {
       return client.getContainers().then(function(res) {
-        expect(res.status).to.be.equal('Success');
+        expect(res.status_code).to.be.equal(200);
         expect(res.metadata).to.contain('/1.0/containers/' + container);
       });
     });
 
     it('Should get a container\'s infos', function() {
       return client.getContainer(container).then(function(res) {
-        expect(res.status).to.be.equal('Success');
+        expect(res.status_code).to.be.equal(200);
         expect(res.metadata.name).to.be.equal(container);
       });
     });
@@ -154,6 +193,15 @@ describe('Main LXD Api endpoints', function() {
       });
     });
 
+  });
+
+  describe('Image deletion', function() {
+    it('Should delete an image', function() {
+      return client.deleteImage(createdImageFingerprint)
+      .then(function(res) {
+        expect(res.status_code).to.be.equal(200);
+      });
+    });
   });
 
 });
